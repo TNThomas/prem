d6acff6a071a404d33260c7861fc5077903383e7
ashfoste <ashton.foster@du.edu>
2023-09-16 23:34:29 -0600

    a few minor tweaks to improve code legibility

--- src/lib/lang/evaluators/order1/parenExpression.ts
+++ src/lib/lang/evaluators/order1/parenExpression.ts
@@ -3,25 +3,24 @@ import { Sequence } from '../../dataStructures'
 import { evalOrderLast } from ".."
 import { ErrorNodeError } from '../errors'
 
-    //for the paren expression we just need to go through everything within the parentheses and evaluate it FIRST before evaluating other things
+    //for the paren expression just go through everything within the parentheses and evaluate it FIRST before evaluating other things
 export function evalParenExpression(src: string, node: TreeCursor): number | Sequence{
 
-    // there is only one child within the parentheses
-        //reminder: node.firstChild SETS node to the first child and it RETURNS a bool 
-        //it does not return the first child like one might initially think
-    if(!node.firstChild()){
-        // currently if empty node.firstChild still returns true so this dosnt work
-       //if node.firstChild is false that means someone wrote "()" which we will treat as a 0 because they look similar
-        return 0;
-    } 
-        if (node.type.isError) {
-            //return 0;
+        // there is only one child within the parentheses
+
+        /*
+        reminder: node.firstChild SETS node to the first child and it RETURNS a bool 
+        it does not return the first child like one might initially think
+        */
+    node.firstChild()
+
+    if (node.type.isError) {
             throw new ErrorNodeError(src, node, "EvaluatorError.")
-        }
-        // we just evaluate the contents of the parentheses as normal using evalOrderLast
+    }
+        // we just evaluate the contents of the parentheses as normal using evalOrderLast 
     let results: number|Sequence = evalOrderLast(src, node)
         // because node.firstChild sets node to the first child, we have to re-set it to the parent here before returning our result
-        node.parent()
-        return results;
+    node.parent()
+    return results;
         
 }
--- tests/lib/lang/evaluators/order1/parenExpression.spec.ts
+++ tests/lib/lang/evaluators/order1/parenExpression.spec.ts
@@ -29,8 +29,9 @@ describe( "evalParenExpression", () => {
         expect(result).toEqual([
             {
                 name: "Output",
-                    //2d4 can be any value between 2 and 8
+                    //2d4 can be any value between 2 and 8 
                     //1d(2d4) thus is 1 die with a face for every possible result from 2d4 
+                    //a very srangely numbered d16 
                 value: [     
                     2,      // 2d4 = 1, 1
                     3, 3,   // 2d4 = 1, 2
@@ -55,6 +56,7 @@ describe( "evalParenExpression", () => {
                     //essentilly ensure there is no difference between 1d2d4 and (1d2)d4
                     // 1d2d4 results in the Sequence {1,2}d4
                     // the end result being the sequence {1d4,2d4}
+                    // we essentially just add the possible results of 1d4 1,2,3,4 to the results of 2d4
                 value: [    
                     1,      // 1d4 = 1
                     2,      // 1d4 = 2  
@@ -76,9 +78,7 @@ describe( "evalParenExpression", () => {
         ])
     })
 
-    // we currently throw an evaluator error in this case, 
-    // but we may want it to result in a zero instead in later versions
-    // () -> 0. 1d() -> [0]
+    // we currently throw an evaluator error in the case that the output is empty
     test("Throws an EvaluatorError if there's nothing between the parentheses", () => {
         let actual = () => evalProgram("output ()")
         expect(actual).toThrowError()
