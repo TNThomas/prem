f08adadd3145da88b5fc99fda5f837662790bcfd
Tabris Thomas <tnthomas748+git@gmail.com>
2023-10-27 12:58:51 -0600

    Switch atLeast and atMost Collations

--- src/lib/resultProcessing/atMostCollation.ts
+++ src/lib/resultProcessing/atMostCollation.ts
@@ -3,7 +3,7 @@ import { exactCollation, type Collation, type ResultType } from ".";
 export function atMostCollation(raw: ResultType): Collation {
     const collation = exactCollation(raw)
     let prev = 0
-    for (const result of [...collation.data.keys()].reverse()) {
+    for (const result of collation.data.keys()) {
         prev += collation.data.get(result) || 0
         collation.data.set(result, prev)
     }
