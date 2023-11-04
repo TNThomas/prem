1d68abdfe59f4176c47423ba9dd21013c4947100
ashfoste <ashton.foster@du.edu>
2023-11-03 19:13:10 -0600

    Multiplication of 2 sequences works now. Created and passed test cases. Multiplication works with both positive and negative numbers, Fixed an issue when multiplying negatives by zero would create -0.

--- src/lib/lang/evaluators/order4/mult.ts
+++ src/lib/lang/evaluators/order4/mult.ts
@@ -8,7 +8,6 @@ import { evalOrderLast } from ".."
 
 export function evalMult(src: string, node: TreeCursor): number | Sequence {
 
-    let result: number | Sequence
     let first: number | Sequence, second: number | Sequence
 
     if (node.firstChild()) {
@@ -33,31 +32,42 @@ export function evalMult(src: string, node: TreeCursor): number | Sequence {
         if (typeof first === "number") {
             //if the second one is a number just multiply them
             if (typeof second === "number") {
-                return first*second;
+                // my original plan was to just do first*second, but an issue arrose in testing 
+                // whenever i would multiply a negative by zero it would produce -0 which would not be deeply equal to the expected zero 
+                // this was the simplest fix i could think of
+                return first*second === 0 ? 0 :first*second;
             }
             // if the first is a number and the second is a sequence, create a new sequence using map where every value in second is multiplied by first  
             if (second instanceof Sequence) {
-                return new Sequence(...second.map(value => (first as number)*value ))
+                return new Sequence(...second.map(value => (first as number)*value === 0 ? 0 : (first as number)*value ))
             }
         }
 
         if (first instanceof Sequence) {
             // if second is just a number we do a similar thing to how we handeled first being a sequnce and second not
             if (typeof second === "number") {
-                return new Sequence(...first.map(value => value*(second as number) ))
+                return new Sequence(...first.map(value => value*(second as number)  === 0 ? 0 : value*(second as number) ))
             }
 
+            // if both are sequences
             if (second instanceof Sequence) {
-                throw new ErrorNodeError(src, node, "Not finished lol.")
 
-                // method: ??? fukin idunno? 
-                //i really dont know what applies here
-                //do i have each value in the first sequence apply to each value in the second? {a(1-n)}*{b(1-n)}={b1*{a1-n}, b2*{a1-n},...,bn*{a1-n}}
-                //do i produce some weird 3d sequence? {a(1-n)}*{b(1-n)}={{b1*a1,b2*a1,...,bn*a1},...,{b1*an,b2*an,...,bn*an}}
-                //do i just multiply the values in the same position on the sequence? {a(1-n)}*{b(1-n)}={a1*b1,a2*b2,...,an*bn}
-                //im really not sure
+                let result: Sequence
+                result = new Sequence();
 
-                    
+                // loop through each value of first
+                first.forEach(firstValue=>{
+                    // create a sequence of every value in second multiplied by the current firstValue and add it to the result sequence
+                    result.insert(new Sequence(...second.map(secondValue => {return firstValue*secondValue === 0 ? 0 :firstValue*secondValue}  )    ))
+                })
+                
+                // return the sequence of sequences
+                return result;
+               
+                //essentially we are producing a 3d sequence: 
+                //{a(1-n)}*{b(1-n)} = {{b1*a1,b2*a1,...,bn*a1},...,{b1*an,b2*an,...,bn*an}}
+                // thanks to how sequences work this results in a single sequence with every possible outcome, which will be desplayed as the probability of each unique outcome
+
             }
 
         }
