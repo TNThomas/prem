71249cb98323c9f942c25c68b6823e26bf42aa1b
Tabris Thomas <tnthomas748+git@gmail.com>
2023-11-27 13:28:39 -0700

    Fix precedence of negation and event sources

--- src/lib/lang/evaluators/order3/index.ts
+++ src/lib/lang/evaluators/order3/index.ts
@@ -1,27 +1,16 @@
-
 import type { TreeCursor } from '@lezer/common'
-import { Cards, Dice, Sequence } from '$lib/lang/dataStructures'
-import { evalEventSource } from './eventSource'
-import { evalOrder2 } from '../order2'
+import { evalOrder2 } from "../order2"
+import type { Sequence } from '$lib/lang/dataStructures'
+import { evalNegative } from './negative'
 
 export function evalOrder3(
     src: string,
     node: TreeCursor
 ): number | Sequence {
-    const results = new Sequence()
-    let events: Cards | Dice | Cards[] | Dice[]
     switch (node.type.name) {
-        case "Cards":
-        case "Dice":
-            events = evalEventSource(src, node)
-            if (!Array.isArray(events)){
-                return events.resultSums
-            }
-            for (const event of events) {
-                results.insert(event.resultSums)
-            }
-            return results
+        case "Negative":
+            return evalNegative(src, node)
         default:
             return evalOrder2(src, node)
-        }
+    }
 }
