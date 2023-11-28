03e392970b7acebc89e45799e5c5f8be577e4293
Tabris Thomas <tntwheels@att.net>
2023-11-27 23:06:12 -0700

    Feature/Mult simplification (#28)

--- src/lib/lang/evaluators/order4/mult.ts
+++ src/lib/lang/evaluators/order4/mult.ts
@@ -26,46 +26,28 @@ export function evalMult(src: string, node: TreeCursor): number | Sequence {
     }
 
     if (first !== undefined && second !== undefined) {
-
-        // if the first one is just a number
-        if (typeof first === "number") {
-            //if the second one is a number just multiply them
-            if (typeof second === "number") {
-                // my original plan was to just do first*second, but an issue arrose in testing 
-                // whenever i would multiply a negative by zero it would produce -0 which would not be deeply equal to the expected zero 
-                // this was the simplest fix i could think of
-                return first*second === 0 ? 0 :first*second;
-            }
-            // if the first is a number and the second is a sequence, create a new sequence using map where every value in second is multiplied by first  
-            if (second instanceof Sequence) {
-                return new Sequence(...second.map(value => (first as number)*value === 0 ? 0 : (first as number)*value ))
-            }
+        // Return a Num when both inputs are Nums
+        if (typeof first === "number" && typeof second === "number") {
+            return multiply(first, second)
         }
 
-        if (first instanceof Sequence) {
-            // if second is just a number we do a similar thing to how we handeled first being a sequnce and second not
-            if (typeof second === "number") {
-                return new Sequence(...first.map(value => value*(second as number)  === 0 ? 0 : value*(second as number) ))
-            }
+        // Ensure first is a sequence so we can do sequence stuff
+        first = typeof first === "number" ? new Sequence(first) : first
+        // Ensure second is a sequence so we can do sequence stuff
+        second = typeof second === "number" ? new Sequence(second) : second
 
-            // if both are sequences
-            if (second instanceof Sequence) {
-                
-                // Calculate the Cartesian product to generate every pair of factors
-                return new Sequence(...cross(
-                    first, second, (a, b) => {
-                        // Then multiply each pair
-                        const product = a * b
-                        return product === 0 ? 0 : product
-                    }
-                ))
-
-            }
-
-        }
-
-
+        // Calculate the Cartesian product to generate every pair of factors, multiplying as we go
+        return new Sequence(...cross(
+            first,
+            second,
+            multiply
+        ))
     }
     throw new EvaluationError(src, node, "Cannot multiply values that are neither Number nor Sequence.")
 
+}
+
+function multiply(a: number, b: number) {
+    const product = a * b
+    return product === 0 ? 0 : product
 }
